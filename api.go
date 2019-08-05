c172454f4e82e62a5d061c4a87cecdb896e1dd8c
Rafael Espinoza <rafael@rafaelespinoza.com>
2019-08-04 18:41:31 -0700

    docs: update godoc

--- api.go
+++ api.go
@@ -1,3 +1,12 @@
+// Package bart is a wrapper for the BART API. It works with JSON, which is
+// still in beta at the time of this writing. See the official BART docs for
+// information https://api.bart.gov/docs/overview/index.aspx
+//
+// The API request example functions are written with vague regard to the
+// output. However, to execute the examples with go test, we must specify some
+// kind of output. Making real requests to the BART API in tests (as opposed to
+// stubbing responses) is meant to expose unexpected errors or panics, which
+// should help make response handling better in the long run.
 package bart
 
 import (
